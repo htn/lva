be39cdfb28f5967b4df73d25e670774e4109ee77
htn <nguyentathuy1986@gmail.com>
2018-10-09 17:51:29 +0700

    vform

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,13 +9,20 @@
 
  window.Vue = require('vue');
 
+ import { Form, HasError, AlertError } from 'vform'
+
+ window.Form = Form;
+ Vue.component(HasError.name, HasError)
+ Vue.component(AlertError.name, AlertError)
+
+
  import VueRouter from 'vue-router';
  Vue.use(VueRouter);
 
  let routes = [
-	 { path: '/dashboard', component: require('./components/Dashboard.vue') },
-	 { path: '/users', component: require('./components/Users.vue') },
-	 { path: '/profile', component: require('./components/Profile.vue') }
+ { path: '/dashboard', component: require('./components/Dashboard.vue') },
+ { path: '/users', component: require('./components/Users.vue') },
+ { path: '/profile', component: require('./components/Profile.vue') }
  ]
 
  const router = new VueRouter({
--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -49,7 +49,26 @@
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
               </div>
               <div class="modal-body">
-                Modal body..
+                <div class="form-group">
+                  <input v-model="form.name" type="text" name="name" placeholder="Name" class="form-control" :class="{ 'is-invalid': form.errors.has('name') }"><has-error :form="form" field="name"></has-error>
+                </div>
+                <div class="form-group">
+                  <input v-model="form.name" type="email" name="email" placeholder="Email Adress" class="form-control" :class="{ 'is-invalid': form.errors.has('email') }"><has-error :form="form" field="email"></has-error>
+                </div>
+                <div class="form-group">
+                  <textarea v-model="form.bio" name="bio" id="bio" placeholder="Short bio for user (Optional)" class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }"></textarea><has-error :form="form" field="bio"></has-error>
+                </div>
+                <div class="form-group">
+                  <select v-model="form.type" name="type" id="type" class="form-control" :class="{'is-invalid': form.errors.has('type')}"><option value="">Select User Role</option>
+                    <option value="admin">Admin</option>
+                    <option value="user">Standar User</option>
+                    <option value="author">Author</option>
+                  </select>
+                  <has-error :form="form" field="type"></has-error>
+                </div>
+                <div class="form-group">
+                  <input v-model="form.password" type="password" name="password" id="password" placeholder="Password" class="form-control" :class="{ 'is-invalid': form.errors.has('password') }"><has-error :form="form" field="password"></has-error>
+                </div>
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
@@ -63,6 +82,18 @@
 
 <script>
     export default {
+        data() {
+            return {
+                form: new Form({
+                    name: '',
+                    email: '',
+                    password: '',
+                    type: '',
+                    bio: '',
+                    photo: ''
+                })
+            }
+        },
         mounted() {
             console.log('Component mounted.')
         }
